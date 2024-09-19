0e9a990fc47422c4e5f251cb7ed134d8b05a3557
eumt07 <eublanmata@gmail.com>
2024-09-19 09:29:49 -0400

    feature: Adding login logic
    Login and reset password were added

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,41 +1,106 @@
-import { HttpException, HttpStatus, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, InternalServerErrorException, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from 'src/modules/prisma/prisma.service';
 import { EmailDto, LoginDto, ResetPasswordDto } from './dto';
+import { comparePassword, encryptPassword } from 'src/shared/utils/index';
+import { JwtService } from '@nestjs/jwt';
 
 
 @Injectable()
 export class AuthService {
     constructor(
-        private readonly prisma: PrismaService
+        private readonly prismaService: PrismaService,
+        private readonly jwtService: JwtService
     ){}
 
     async login({email, password}: LoginDto){
         try {
-            console.log("Email: ", email);
-            console.log("Password: ", password); 
+            let user: { id:string, password:string, role:string}
+
+            //Searching user
+            user = await this.prismaService.user.findUnique({
+                where: { email },
+                select: {
+                    id: true,
+                    password: true,
+                    role: true
+                }
+            })
+
+            if(!user) {
+                throw new UnauthorizedException('Invalid Credentials');
+            }
+
+            const isPasswordCorrect = comparePassword(password,user.password);
+
+            if(!isPasswordCorrect){
+                throw new UnauthorizedException("Invalid Credentials")
+            }
+
+            //Creating Token
+            const payload = {id: user.id, role: user.role}
+
+            const token = await this.jwtService.signAsync(payload);
+
+            return{
+                status: 201,
+                token
+            }
+
         } catch (error) {
             throw new NotFoundException("Invalid Credentials");
         }
     }
     async createEmailToken({email}: EmailDto){
         try {
-            console.log("Email: ",email);
-            
+            let user: {id:string, email:string, username:string, firstName:string, lastName:string};
+
+            user = await this.prismaService.user.findUnique({
+                where: { email},
+                select: {
+                    id: true,
+                    email: true,
+                    username: true,
+                    firstName: true,
+                    lastName: true
+                },
+            });
+        
+            if (!user) {
+                throw new UnauthorizedException('Invalidad Email');
+            }
+        
+            //Sent email
+
+            return {
+                ok: "true",
+                status: "201",
+                message: "We have sent you an email to recovery your password.!!"
+            };
+
+
         } catch (error) {
             throw new NotFoundException(error.message)
         }
     }
-    async resetPassword(userId: string, {newPassword, newConfirmPassword}: ResetPasswordDto){
+    async resetPassword(userId: string,  {newPassword, newConfirmPassword}: ResetPasswordDto){
         try {
             if(newPassword !== newConfirmPassword){
                 throw new HttpException("Password are different", HttpStatus.NOT_FOUND)
             }
 
-            console.log(`
-                password: ${newPassword}\n
-                confirmPassword: ${newConfirmPassword}\n
-                userId: ${userId}
-                `);
+            //Password hashed
+            const passwordHashed = encryptPassword(newPassword);
+
+            await this.prismaService.user.update({
+                where: { id: userId },
+                data: { password: passwordHashed }
+            })
+
+            return {
+                ok: 'true',
+                status: '201',
+                message: 'Your Password has been changed!!',
+            };
             
         } catch (error) {
             throw new InternalServerErrorException(error.message);
