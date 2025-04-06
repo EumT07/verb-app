18bf9ece572621eec5dce284425aac81fc6b0479
eumt07 <eublanmata@gmail.com>
2025-04-06 15:03:58 -0400

    Changing prompt AI in order to get a correct info from verb stative

--- src/modules/deepseek-ai/deepseek-ai.service.ts
+++ src/modules/deepseek-ai/deepseek-ai.service.ts
@@ -23,20 +23,18 @@ export class DeepseekAiService {
             messages: [
                 {
                     role: 'system',
-                    content: `I'm going to provide you a verb, and I just want you create sentences, you must include sentences in third person like: he, she and it, using the correct form of the verb and I hope get at least an array with thre or four exercises not longer than 60 characteres, and don't include questions.!!
-
-                    Example INPUT: To Do
+                    content: `Generate short sentences (max 60 chars each) in third-person (he/she/it)
+                        For Action verbs: Dynamic/active usage.
+                        For Stative verbs: Natural, native-like usage. If unnatural in progressive tenses, use gerund (ing) as subject (e.g., "Loving her is easy").
 
                     Example JSON OUTPUT:
 
                     {
-                        "verb": "To Do",
                         "sentences" : {
                             "present": [four exercises],
                             "past": [four exercises],
                             "past_Participle": [four exercises],
-                            "present_Participle": [four exercises in present or past with present participle verb form],
-                            "future": [four exercises]
+                            "present_Participle": [four exercises]
                         }
                     }`,
                 },
